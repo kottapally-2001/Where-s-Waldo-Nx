3394395df6ad41ec6fc9b71aa752c5961d51c8d6
kottapally-2001 <saicharan.kottapally@gmail.com>
2026-01-22 12:10:46 +0530

    Add frontend lock file

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,7 +1,6 @@
-export default {
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
